9b350577be0f476e8fca762c6e3526ee145593b1
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-14 23:05:22 -0400

    Looping through the users in the state to pass the state with props. Passed in each individual user to map through and create a list.

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-    state = {
-        id: "1",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-    };
     render() {
-        const { login, avatar_url, html_url } = this.state;
+        const { login, avatar_url, html_url } = this.props.user;
 
         return (
             <div className="card text-center">
-                <img src={this.state.avatar_url} alt="" className="round-img" style={{ width: "60px"}}/>
-            <h3>{this.state.login}</h3>
+                <img src={avatar_url} alt="" className="round-img" style={{ width: "60px"}}/>
+            <h3>{login}</h3>
 
             <div>
                 <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import UserItem from "./UserItem";
 
 class Users extends Component {
     state = {
@@ -28,7 +29,7 @@ class Users extends Component {
         return (
             <div>
                 {this.state.users.map(user => (
-                    <div key={user.id}>{user.login}</div>
+                    <UserItem key={user.id} user={user} />
                 ))}
             </div>
         )
