69b870669c52e9b304964f3ec1b40280b16d28cc
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-14 23:12:16 -0400

    Added a container to the displayed users.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
+          <div className="container">
         <Users />
+        </div>
       </div>
     );
   }
