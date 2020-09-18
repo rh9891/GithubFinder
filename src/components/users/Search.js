5dce0b8fc9ac63303e928eccc8da09def8a12afd
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-18 18:08:19 -0400

    Set up an alert for Search component. Created an Alert component. Manipulated the state, so that the alert only displays if no entry is filled in when searching for Github users.

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,13 +9,18 @@ class Search extends Component {
   static propTypes = {
       searchUsers: PropTypes.func.isRequired,
       clearUsers: PropTypes.func.isRequired,
-      showClear: PropTypes.bool.isRequired
+      showClear: PropTypes.bool.isRequired,
+      setAlert: PropTypes.func.isRequired
   };
 
   onSubmit = event => {
       event.preventDefault();
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
+      if (this.state.text === "") {
+          this.props.setAlert("Please enter a search term.", "light");
+      } else {
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: "" });
+      }
   };
 
   onChange = event => this.setState({ [event.target.name]: event.target.value });
