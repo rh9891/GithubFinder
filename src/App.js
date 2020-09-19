e25c3d6de6a6ce010fa3b31cd8c98bfe10560d04
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-18 23:00:15 -0400

    Added to the file structure. Created an About component. Set a path to the About component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+
 import axios from "axios";
 import "./App.css";
 
@@ -49,6 +51,7 @@ class App extends Component {
                   <Users loading={loading} users={users} />
                 </Fragment>
               )} />
+              <Route exact path="/about" component={About} />
             </Switch>
             
         </div>
