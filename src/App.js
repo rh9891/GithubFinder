c0b7e79374e44c9c9149af3d663b11a3d230edaf
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-18 22:27:12 -0400

    Imported react-router-dom. Set a route for the Search/Users component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
@@ -36,14 +37,23 @@ class App extends Component {
     const { users, loading } = this.state;
 
     return (
+      <Router>
       <div className="App">
         <Navbar />
           <div className="container">
             <Alert alert={this.state.alert} />
-            <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false } setAlert={this.setAlert} /> 
-            <Users loading={loading} users={users} />
+            <Switch>
+              <Route exact path="/" render={props => (
+                <Fragment>
+                  <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false } setAlert={this.setAlert} /> 
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )} />
+            </Switch>
+            
         </div>
       </div>
+      </Router>
     );
   }
 }
