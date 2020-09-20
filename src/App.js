d728673ef0030bf29de0a039634c8f7496f5f9d0
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-09-19 23:52:22 -0400

    Added single user component to the application.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
@@ -12,6 +13,7 @@ import "./App.css";
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -25,6 +27,15 @@ class App extends Component {
     this.setState({ users: response.data.items, loading: false });
   };
 
+  // Gets information for single Github user.
+  getUser = async username => {
+    this.setState({ loading: true });
+ 
+    const response = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
+    this.setState({ users: response.data, loading: false });
+  }
+
   // Clears users from state.
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -36,7 +47,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, user } = this.state;
 
     return (
       <Router>
@@ -52,6 +63,9 @@ class App extends Component {
                 </Fragment>
               )} />
               <Route exact path="/about" component={About} />
+              <Route exact path="/user/:login" render={props => (
+                <User { ...props } getUser={this.getUser} user={user} loading={loading} />
+              )} />
             </Switch>
             
         </div>
